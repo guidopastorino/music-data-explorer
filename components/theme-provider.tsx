f9fff6ecef561d27a942ca839ac2a37471d56a0e
Guido Pastorino <guidopasto05@gmail.com>
2025-11-11 14:06:23 -0300

    Add next-themes dependency and implement theme provider; update global styles and layout

---
+++ components/theme-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import type { ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      storageKey="wollen-theme"
+      defaultTheme="system"
+      attribute="class" {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
+}
